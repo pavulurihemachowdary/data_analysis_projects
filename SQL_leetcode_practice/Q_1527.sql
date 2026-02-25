7f9ff78e14f1f463061e03811e81f85ccc94f8dd
pavulurihemachowdary <20bq1a05i4@vvit.net>
2026-02-25 09:33:55 +0530

    added sql 1527 solution

---
+++ SQL_leetcode_practice/Q_1527.sql
@@ -0,0 +1,55 @@
+--Description
+
+-- Table: Patients
+
+-- +--------------+---------+
+-- | Column Name  | Type    |
+-- +--------------+---------+
+-- | patient_id   | int     |
+-- | patient_name | varchar |
+-- | conditions   | varchar |
+-- +--------------+---------+
+-- patient_id is the primary key (column with unique values) for this table.
+-- 'conditions' contains 0 or more code separated by spaces. 
+-- This table contains information of the patients in the hospital.
+ 
+
+-- Write a solution to find the patient_id, patient_name, and conditions of the patients who have Type I Diabetes. Type I Diabetes always starts with DIAB1 prefix.
+
+-- Return the result table in any order.
+
+-- The result format is in the following example.
+
+ 
+
+-- Example 1:
+
+-- Input: 
+-- Patients table:
+-- +------------+--------------+--------------+
+-- | patient_id | patient_name | conditions   |
+-- +------------+--------------+--------------+
+-- | 1          | Daniel       | YFEV COUGH   |
+-- | 2          | Alice        |              |
+-- | 3          | Bob          | DIAB100 MYOP |
+-- | 4          | George       | ACNE DIAB100 |
+-- | 5          | Alain        | DIAB201      |
+-- +------------+--------------+--------------+
+-- Output: 
+-- +------------+--------------+--------------+
+-- | patient_id | patient_name | conditions   |
+-- +------------+--------------+--------------+
+-- | 3          | Bob          | DIAB100 MYOP |
+-- | 4          | George       | ACNE DIAB100 | 
+-- +------------+--------------+--------------+
+-- Explanation: Bob and George both have a condition that starts with DIAB1.
+
+--we can solve this using LIKE or REGEXP_LIKE string comparisons 
+select patient_id,patient_name,conditions
+from Patients
+where conditions like 'DIAB1%' or
+conditions like '% DIAB1%'
+--we can use, where REGEXP_LIKE(conditions,'(^| )DIAB1')
+
+/* Write your PL/SQL query statement below */
+delete from Person p where p.email in (select email from person p1 where p.id > p1.id);
